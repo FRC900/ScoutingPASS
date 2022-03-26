1d370c87482908a2163868e85953c18f9834ee88
Shikha Srivastava <ssriva@us.ibm.com>
2022-03-26 14:40:02 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -62,7 +62,6 @@ var config_data = `
         "code":"as",
         "title": "Auto Start Position",
         "type":"field_image",
-       // "filename":"2022/field_image.png"
         "filename":"2022/0.png"
       }
        
