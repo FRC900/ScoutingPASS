4bbd9998917648890906b80a750864eb7baf52b6
Theo-Reeves <54045165+Theo-Reeves@users.noreply.github.com>
2023-03-03 23:44:21 -0500

    update to unca event

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023isde2",  
+      "defaultValue": "2023ncash",  
       "required": "true",
       "disabled": "true"
     },
