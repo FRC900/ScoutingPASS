07d41cae6b6f92bc6c39f7c0259e90a3b8d360c6
RoboticsTeam900 <roboticsteam900@gmail.com>
2023-03-30 17:48:02 -0400

    change to dcmp

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ncwak",  
+      "defaultValue": "2023nccmp",  
       "required": "true",
       "disabled": "true"
     },
