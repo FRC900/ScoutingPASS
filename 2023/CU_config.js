827018bfec0933d05e140725e307f54a20b8e956
RoboticsTeam900 <roboticsteam900@gmail.com>
2023-04-15 13:45:31 -0400

    Michigan FORD event key

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023nccmp",  
+      "defaultValue": "2023micmp2",  
       "required": "true",
       "disabled": "true"
     },
