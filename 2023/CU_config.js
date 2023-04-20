de398f72dad7784587a67ca4544c73ca2bcc33d0
RoboticsTeam900 <roboticsteam900@gmail.com>
2023-04-19 20:01:09 -0500

    Ventura, CA regional event key

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023new",  
+      "defaultValue": "2023cave",  
       "required": "true",
       "disabled": "true"
     },
