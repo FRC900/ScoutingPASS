b40825bd84ca3ef1597540777936057ac983cbc5
Megan Massengill <forlornlawngnome@gmail.com>
2024-01-20 16:37:15 -0500

    put in an event

--- 2024/CU_config.js
+++ 2024/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024new",  
+      "defaultValue": "2024isde1",  
       "required": "true",
       "disabled": "true"
     },
