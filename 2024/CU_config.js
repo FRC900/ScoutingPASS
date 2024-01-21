cf1b2394dd0b5322b137cdced0c3e5d0e2fd6936
Megan Massengill <forlornlawngnome@gmail.com>
2024-01-21 15:39:53 -0500

    debugging

--- 2024/CU_config.js
+++ 2024/CU_config.js
@@ -159,7 +159,7 @@ var config_data = `
       "type": "cycle"
     },
     { "name": "Climb Final State",
-      "code": "t_climb_state",
+      "code": "ad",
       "type":"radio",
       "choices": {
         "d": "Docked (not Engaged)<br>",
