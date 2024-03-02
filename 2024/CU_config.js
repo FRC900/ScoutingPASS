0b35809d991430fb65984f89322a1c4b9ac217ee
Meg Massengill <forlornlawngnome@gmail.com>
2024-03-02 07:42:14 -0500

    Update CU_config.js
    Change to orange county event

--- 2024/CU_config.js
+++ 2024/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mrcmp",  
+      "defaultValue": "2024ncwak",  
       "required": "true",
       "disabled": "true"
     },
