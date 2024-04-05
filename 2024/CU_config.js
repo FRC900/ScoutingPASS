d3f319d68d737f33182f0a8a309dceee72407479
Megan Massengill <forlornlawngnome@gmail.com>
2024-04-05 16:21:17 -0400

    update to dcmp

--- 2024/CU_config.js
+++ 2024/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024ncash",  
+      "defaultValue": "2024nccmp",  
       "required": "true",
       "disabled": "true"
     },
